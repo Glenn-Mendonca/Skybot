8f848df3a95af55ae9dbe7dbf95af351501d60b1
Skyhero-admin <smnouman007@gmail.com>
2021-06-27 17:27:15 +0530

    first commit

---
+++ Skybot.py
@@ -0,0 +1,32 @@
+import discord
+import os
+import requests
+import json
+import random
+
+client = discord.Client()
+
+def get_quote():
+  response = requests.get("https://zenquotes.io/api/random")
+  jd= json.loads(response.text)
+  quote = jd[0]['q']
+  return quote
+
+@client.event
+async def on_ready():
+  print('I am ready as {0.user}'.format(client))
+
+@client.event
+async def on_message(message):
+  if message.author == client.user:
+    return
+
+  if message.startswith('hey' or 'hello'):
+    await message.channel.send('Hello Sir')
+
+  if message.startswith('inspire'):
+    await message.channel.send(get_quote)
+
+client.run(os.environ['token'])
+
+
